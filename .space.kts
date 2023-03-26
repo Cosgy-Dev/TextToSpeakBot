e806cdf70360f7cc666f09867681ce14a76dcd08
Sakuya Kosugi <kosukisaki@yahoo.co.jp>
2023-03-26 18:44:52 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,19 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Qodana") {
+   container("jetbrains/qodana-jvm") {
+      env["QODANA_TOKEN"] = Secrets("qodana-token")
+      shellScript {
+         content = """
+            QODANA_REMOTE_URL="ssh://git@git.${'$'}JB_SPACE_API_URL/${'$'}JB_SPACE_PROJECT_KEY/${'$'}JB_SPACE_GIT_REPOSITORY_NAME.git" \
+            QODANA_BRANCH=${'$'}JB_SPACE_GIT_BRANCH \
+            QODANA_REVISION=${'$'}JB_SPACE_GIT_REVISION \
+            qodana
+         """.trimIndent()
+      }
+   }
+}
